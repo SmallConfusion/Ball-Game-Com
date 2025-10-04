8e5ba2ea53c39dc05c22597a09d41a15578f72dc
Fusion <103614941+SmallConfusion@users.noreply.github.com>
2025-10-04 14:11:01 -0500

    Bug fixes

--- ship.gd
+++ ship.gd
@@ -1,14 +1,32 @@
 class_name Ship
 extends Area2D
 
+@onready var end_menu: PanelContainer = %EndMenu
+@onready var restart_button: Button = $"../CanvasLayer/EndMenu/MarginContainer/VBoxContainer/RestartButton"
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	area_entered.connect(func(_x): _died())
+	end_menu.visible = false
+	restart_button.pressed.connect(func():
+		get_tree().paused = false
+		get_tree().reload_current_scene()
+	)
+	
+	position = get_global_mouse_position()
+	reset_physics_interpolation()
 
 func _physics_process(_delta: float) -> void:
+	var old_position := position
 	position = get_global_mouse_position()
+	var ray_param := PhysicsRayQueryParameters2D.create(old_position, position)
+	ray_param.collide_with_areas = true
+	var collisions := get_world_2d().direct_space_state.intersect_ray(ray_param)
+	
+	if not collisions.is_empty():
+		_died()
 
 func _died() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	get_tree().paused = true
+	end_menu.visible = true
